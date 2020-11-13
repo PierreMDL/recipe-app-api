a2f58f08ce388fec1abfa5dc34b13a92ada516d2
PierreMDL <pierresimon.mdl@gmail.com>
2020-11-13 18:54:21 +0000

    Adding user, superusers

--- app/app/calc.py
+++
@@ -1,8 +0,0 @@
-def add(x, y):
-    """Adds two numbers"""
-    return x + y
-
-
-def substract(x, y):
-    """Substracts one number from another"""
-    return x - y
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_model(self):
+        """Test creation of user model with email successful"""
+        email = "paulo@modulo.fr"
+        password = "TestPass1234"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_super_user_model(self):
+        """Test creation of super user"""
+        user = get_user_model().objects.create_super_user(
+            "admin@modulo.fr",
+            "passTest1234"
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+    def test_new_user_email_normalized(self):
+        """Test new email is normalized"""
+        email = "paulo@MODULO.FR"
+        user = get_user_model().objects.create_user(email, "1234")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_email_address_provided(self):
+        """Test an error is given if user doesn't provide email address"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', "1234")
